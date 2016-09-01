a306f3331704b080a33ff4c6cd7e7cdbb00ea32a
TonyPhipps <anthony.james.phipps@gmail.com>
2016-09-01 10:31:34 -0400

    Options notes added.

--- DaftUI/daftAutomaton/Options.lua
+++ DaftUI/daftAutomaton/Options.lua
@@ -8,28 +8,28 @@ addonTable.SELL_GREYS = true;
 
 
 -- Friend play / multiboxing
-addonTable.ACCEPT_GROUP = true;
-addonTable.ACCEPT_QUESTS = true; -- Only when in a group
-addonTable.ACCEPT_SUMMON = true;
-addonTable.FOLLOW = true;
+addonTable.ACCEPT_GROUP = true; -- from friends / guildies
+addonTable.ACCEPT_QUESTS = false; -- Only when in a group
+addonTable.ACCEPT_SUMMON = true; -- when not in combat
+addonTable.FOLLOW = true; -- from friends / guildies
 addonTable.INVITE = false;
-addonTable.INVITE_AUTORAID = false;
+addonTable.INVITE_AUTORAID = false; -- convert to raid when group is large
 addonTable.INVITE_TRIGGER = "invite";
 addonTable.INVITE_VERBOSE = true; -- trigger can exist anywhere in message
-addonTable.PROMOTE_ASSISTANT = true;
-addonTable.PROMOTE_LEADER = true;
+addonTable.PROMOTE_ASSISTANT = true; -- from friends / guildies
+addonTable.PROMOTE_LEADER = true; -- from friends / guildies
 addonTable.SHARE_QUESTS = true;
 
 
 -- PVP
-addonTable.ACCEPT_RESURRECT = true;
+addonTable.ACCEPT_RESURRECT = true; -- when not in combat
 addonTable.RELEASE_PVP = true;
 addonTable.RELEASE_WORLD = true;
 
 
 -- Miscellaneous
 addonTable.SCREENSHOT_ACHIEVEMENTS = true;
-addonTable.TOGGLE_CINEMATIC_SOUND = true;
+addonTable.TOGGLE_CINEMATIC_SOUND = true; -- enable sound for cinematic, revert settings afterward
 
 
 
