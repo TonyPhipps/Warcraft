e12879169b55fc954df911b83992c93cc83468b4
TonyPhipps <anthony.james.phipps@gmail.com>
2016-09-13 15:36:52 -0400

    Updates.

--- DaftUI/daftMainBar/Options.lua
+++ DaftUI/daftMainBar/Options.lua
@@ -1,13 +1,8 @@
 local addonName, addonTable = ... ;
 
+addonTable.MAIN_BAR_SCALE = .9;
+
+addonTable.HIDE_ART = false;
 addonTable.HIDE_GRYPHONS = false;
-addonTable.HIDE_ICON_BGS = false;
-
-addonTable.BOTTOM_LEFT_BAR = 1;
-addonTable.PET_BAR = 2;
-addonTable.BOTTOM_RIGHT_BAR = 3;
-
-addonTable.MAIN_BAR_SCALE = 1.0;
-
 
 
--- DaftUI/daftMainBar/daftMainBar.lua
+++ DaftUI/daftMainBar/daftMainBar.lua
@@ -1,10 +1,6 @@
 local addonName, addonTable = ... ;
 local addon = CreateFrame("Frame");
-
-local point1, relativeTo1, relativePoint1, xOffset1, yOffset1 = "CENTER", MainMenuBar, "CENTER", -254, 58;
-local point2, relativeTo2, relativePoint2, xOffset2, yOffset2 = "CENTER", MainMenuBar, "CENTER", 258, 58;
-local point3, relativeTo3, relativePoint3, xOffset3, yOffset3 = "CENTER", MainMenuBar, "CENTER", 258, -4;
-
+local NoTexture = CreateFrame("Frame", nil);
 
 addon:RegisterEvent("PLAYER_ENTERING_WORLD");
 
@@ -33,28 +29,11 @@ local bagButtons = {
 
 ---- HELPER FUNCTIONS ----
 
-function addon:SetAllScriptsToHide(frame)
-	local scripts = {
-		"OnShow",
-		"OnHide",
-		"OnEvent",
-		"OnEnter",
-		"OnLeave",
-		"OnUpdate",
-		"OnValueChanged",
-		"OnClick",
-		"OnMouseDown",
-		"OnMouseUp"
-	};
-	
-	for i, script in next, scripts do
-		frame:Hide();
+function addon:ScaleFrames(frames, scale)
 		
-		if frame:HasScript(script) then
-			frame:SetScript(script, function()
-				frame:Hide();
-			end);
-		end;
+	for i, frame in next, frames do
+		local thisFrame = _G[frame];
+		thisFrame:SetScale(scale);
 	end;
 end;
 
@@ -81,15 +60,6 @@ function addon:FadeFramesOut(frames)
 end;
 
 
-function addon:ScaleFrames(frames, scale)
-		
-	for i, frame in next, frames do
-		local thisFrame = _G[frame];
-		thisFrame:SetScale(scale);
-	end;
-end;
-
-
 function addon:FadeFrames(frames)
 	
 	for i, frame in next, frames do
@@ -108,106 +78,209 @@ end;
 
 ---- FUNCTIONS ----
 
-
-function addon:MoveMultiBarBottomRight()
-	MultiBarBottomRight:ClearAllPoints();	
+function addon:ResizeMainBar()
+	for _, texture in next, {
+	MainMenuBarTexture2, MainMenuBarTexture3,
+	MainMenuBarPageNumber, ActionBarUpButton, ActionBarDownButton, 						--button of Bar Number, up, down
+	StanceBarLeft, StanceBarMiddle, StanceBarRight, } do								--BG of Stance
+		texture:SetParent(NoTexture);
+		NoTexture:Hide();
+	end;
 	
-	if addonTable.BOTTOM_RIGHT_BAR == 1 then
-		MultiBarBottomRight:SetPoint(point1, relativeTo1, relativePoint1, xOffset1, yOffset1);
-	elseif addonTable.BOTTOM_RIGHT_BAR == 2 then
-		MultiBarBottomRight:SetPoint(point2, relativeTo2, relativePoint2, xOffset2, yOffset2);
-	elseif addonTable.BOTTOM_RIGHT_BAR == 3 then
-		MultiBarBottomRight:SetPoint(point3, relativeTo3, relativePoint3, xOffset3, yOffset3);
-	else end;
+	for _, bar in next, {
+	MainMenuBar, MainMenuBarArtFrame, 
+	MainMenuExpBar, MainMenuBarMaxLevelBar,
+	ReputationWatchBar, ReputationWatchBar.StatusBar,
+	HonorWatchBar, HonorWatchBar.StatusBar,
+	ArtifactWatchBar, ArtifactWatchBar.StatusBar,}  do
+		bar:SetWidth(bar:GetWidth()/2);
+	end;
+	
+	for i = 10,19 do
+		_G["MainMenuXPBarDiv"..i]:SetParent(NoTexture);
+	end;
+	
+	ReputationWatchBar.StatusBar.WatchBarTexture0:SetWidth(128);
+	ReputationWatchBar.StatusBar.WatchBarTexture1:SetWidth(128);
+	ReputationWatchBar.StatusBar.WatchBarTexture2:SetWidth(128);
+	ReputationWatchBar.StatusBar.WatchBarTexture3:SetWidth(128);
+
+	ArtifactWatchBar.StatusBar.WatchBarTexture0:SetWidth(128);
+	ArtifactWatchBar.StatusBar.WatchBarTexture1:SetWidth(128);
+	ArtifactWatchBar.StatusBar.WatchBarTexture2:SetWidth(128);
+	ArtifactWatchBar.StatusBar.WatchBarTexture3:SetWidth(128);
+
+	HonorWatchBar.StatusBar.WatchBarTexture0:SetWidth(128);
+	HonorWatchBar.StatusBar.WatchBarTexture1:SetWidth(128);
+	HonorWatchBar.StatusBar.WatchBarTexture2:SetWidth(128);
+	HonorWatchBar.StatusBar.WatchBarTexture3:SetWidth(128);
+
+	ReputationWatchBar.StatusBar.XPBarTexture0:SetWidth(128);
+	ReputationWatchBar.StatusBar.XPBarTexture1:SetWidth(128);
+	ReputationWatchBar.StatusBar.XPBarTexture2:SetWidth(128);
+	ReputationWatchBar.StatusBar.XPBarTexture3:SetWidth(128);
+
+	ArtifactWatchBar.StatusBar.XPBarTexture0:SetWidth(128);
+	ArtifactWatchBar.StatusBar.XPBarTexture1:SetWidth(128);
+	ArtifactWatchBar.StatusBar.XPBarTexture2:SetWidth(128);
+	ArtifactWatchBar.StatusBar.XPBarTexture3:SetWidth(128);
+
+	HonorWatchBar.StatusBar.XPBarTexture0:SetWidth(128);
+	HonorWatchBar.StatusBar.XPBarTexture1:SetWidth(128);
+	HonorWatchBar.StatusBar.XPBarTexture2:SetWidth(128);
+	HonorWatchBar.StatusBar.XPBarTexture3:SetWidth(128);
+
+	MainMenuBar:ClearAllPoints();
+	MainMenuBar:SetPoint("BOTTOM", WorldFrame, "BOTTOM", 0, 0);
+	MainMenuBar.SetPoint = function() end;
+	
+	MainMenuBarTexture0:ClearAllPoints();
+	MainMenuBarTexture0:SetPoint("RIGHT", MainMenuBar, "CENTER", 0, -4);
+	MainMenuBarTexture0.SetPoint = function() end;
+	
+	MainMenuBarTexture1:ClearAllPoints();
+	MainMenuBarTexture1:SetPoint("LEFT", MainMenuBar, "CENTER", 0, -4);
+	MainMenuBarTexture1.SetPoint = function() end;
+	
+	MainMenuBarLeftEndCap:ClearAllPoints();
+	MainMenuBarLeftEndCap:SetPoint("BOTTOMRIGHT", MainMenuBar, "BOTTOMLEFT", 40, 0);
+	MainMenuBarLeftEndCap.SetPoint = function() end;
+	
+	MainMenuBarRightEndCap:ClearAllPoints();
+	MainMenuBarRightEndCap:SetPoint("BOTTOMLEFT", MainMenuBar, "BOTTOMRIGHT", -40, 0);
+	MainMenuBarRightEndCap.SetPoint = function() end;
 end;
 
 
-function addon:MoveMultiBarBottomLeft()
+function addon:MoveBarFrames()
+	
 	MultiBarBottomLeft:ClearAllPoints();
-	
-	if addonTable.BOTTOM_LEFT_BAR == 1 then
-		MultiBarBottomLeft:SetPoint(point1, relativeTo1, relativePoint1, xOffset1, yOffset1);
-	elseif addonTable.BOTTOM_LEFT_BAR == 2 then
-		MultiBarBottomLeft:SetPoint(point2, relativeTo2, relativePoint2, xOffset2, yOffset2);
-	elseif addonTable.BOTTOM_LEFT_BAR == 3 then
-		MultiBarBottomLeft:SetPoint(point3, relativeTo3, relativePoint3, xOffset3, yOffset3);
-	else end;
+	MultiBarBottomLeft:SetPoint("BOTTOM", ReputationWatchBar, "TOP", 0, 5);
 	MultiBarBottomLeft.SetPoint = function() end;
-end;
-
-
-function addon:MovePetActionBarFrame()
-	PetActionBarFrame:ClearAllPoints();
 	
-	if addonTable.PET_BAR == 1 then
-		PetActionBarFrame:SetPoint(point1, relativeTo1, relativePoint1, xOffset1 + 30, yOffset1);
-	elseif addonTable.PET_BAR == 2 then
-		PetActionBarFrame:SetPoint(point2, relativeTo2, relativePoint2, xOffset2 + 30, yOffset2);
-	elseif addonTable.PET_BAR == 3 then
-		PetActionBarFrame:SetScale(1.2);
-		PetActionBarFrame:SetPoint(point3, relativeTo3, relativePoint3, xOffset3 - 15, yOffset3 + 5);
-		MainMenuBarTexture2:SetTexture(CharacterFrameInsetRightBg:GetTexture());
-		MainMenuBarTexture2:SetTexCoord(CharacterFrameInsetRightBg:GetTexCoord());
-		MainMenuBarTexture3:SetTexture(CharacterFrameInsetRightBg:GetTexture());
-		MainMenuBarTexture3:SetTexCoord(CharacterFrameInsetRightBg:GetTexCoord());
-	else end;
-	PetActionBarFrame.SetPoint = function() end;
+	MultiBarBottomRight:ClearAllPoints();
+	MultiBarBottomRight:SetPoint("BOTTOM", MultiBarBottomLeft, "TOP", 0, 5);
+	MultiBarBottomRight.SetPoint = function() end;
+	
+	
+	MultiBarRight:ClearAllPoints();
+	MultiBarRight:SetPoint("RIGHT", WorldFrame, "RIGHT", 0, 0);
+	MultiBarRight.SetPoint = function() end;
+
+	
+	if StanceBarFrame:IsShown() then
+		StanceBarFrame:ClearAllPoints();
+		
+		if MultiBarBottomRight:IsShown() then	
+			StanceBarFrame:SetPoint("BOTTOMLEFT", MultiBarBottomRight, "TOPLEFT", -10, 5);
+		
+		elseif MultiBarBottomLeft:IsShown() then
+			StanceBarFrame:SetPoint("BOTTOMLEFT", MultiBarBottomLeft, "TOPLEFT", -10, 5);
+		
+		else
+			StanceBarFrame:SetPoint("BOTTOMLEFT", ReputationWatchBar, "TOPLEFT", -10, 5);
+		end;
+		
+		
+		StanceBarFrame.SetPoint = function() end;
+	end;
+	
+	if PetActionBarFrame:IsShown() then	
+		PetActionBarFrame:ClearAllPoints();
+		
+		if MultiBarBottomRight:IsShown() then	
+			PetActionBarFrame:SetPoint("BOTTOMRIGHT", MultiBarBottomRight, "TOPRIGHT", 100, 5);
+		
+		elseif MultiBarBottomLeft:IsShown() then
+			PetActionBarFrame:SetPoint("BOTTOMRIGHT", MultiBarBottomLeft, "TOPRIGHT", 100, 5);
+		
+		else
+			PetActionBarFrame:SetPoint("BOTTOMRIGHT", ReputationWatchBar, "TOPRIGHT", 0, 5);
+		end;
+		
+		PetActionBarFrame.SetPoint = function() end;
+		PetActionBarFrame:SetScale(.8);
+	end;
 end;
-		
-		
+
+
+function addon.HideBlizzardArt()
+	NoTexture:Hide();
+	
+	MainMenuBarLeftEndCap:SetParent(NoTexture);
+	MainMenuBarRightEndCap:SetParent(NoTexture);
+	
+	for i = 0,3  do
+		_G["MainMenuBarTexture"..i]:SetParent(NoTexture);
+		_G["MainMenuMaxLevelBar"..i]:SetParent(NoTexture);
+		ReputationWatchBar.StatusBar["WatchBarTexture"..i]:SetParent(NoTexture);
+		ArtifactWatchBar.StatusBar["WatchBarTexture"..i]:SetParent(NoTexture);
+		HonorWatchBar.StatusBar["WatchBarTexture"..i]:SetParent(NoTexture);
+		ReputationWatchBar.StatusBar["XPBarTexture"..i]:SetParent(NoTexture);
+		ArtifactWatchBar.StatusBar["XPBarTexture"..i]:SetParent(NoTexture);
+		HonorWatchBar.StatusBar["XPBarTexture"..i]:SetParent(NoTexture);
+	end;
+	
+	for i = 1,2  do
+		_G["PossessBackground"..i]:SetParent(NoTexture);
+	end;
+	
+	for i = 0,1  do
+		_G["SlidingActionBarTexture"..i]:SetParent(NoTexture);
+	end;
+	
+	for i = 1,19 do
+		_G["MainMenuXPBarDiv"..i]:SetParent(NoTexture);
+	end;
+	
+	for _, texture in next, {
+	MainMenuBarPageNumber, ActionBarUpButton, ActionBarDownButton, 						--button of Bar Number, up, down
+	MainMenuXPBarTextureLeftCap, MainMenuXPBarTextureRightCap, MainMenuXPBarTextureMid, --Art between xp and bar1	
+	StanceBarLeft, StanceBarMiddle, StanceBarRight, } do								--BG of Stance
+		texture:SetParent(NoTexture);
+	end;
+end;
+
+
 ---- MAIN ----
 
+function addon.Main()
+
+	if addonTable.HIDE_GRYPHONS then
+		MainMenuBarLeftEndCap:SetParent(NoTexture);
+		MainMenuBarRightEndCap:SetParent(NoTexture);
+		NoTexture:Hide();
+	end;
+	
+	if addonTable.HIDE_ART then
+		addon.HideBlizzardArt();
+	end;
+	
+	addon:MoveBarFrames();
+	addon:ResizeMainBar();
+	
+	-- Scale MainMenuBar
+	MainMenuBar:SetScale(addonTable.MAIN_BAR_SCALE);
+
+	-- Main Menu
+	CharacterMicroButton:ClearAllPoints();
+	CharacterMicroButton:SetPoint("TOPLEFT", ReputationWatchBar, "TOPLEFT", 0, 30);
+	addon:ScaleFrames(menuButtons, .7);
+	addon:FadeFrames(menuButtons);
+
+	-- Bags
+	MainMenuBarBackpackButton:ClearAllPoints();
+	MainMenuBarBackpackButton:SetPoint("TOPRIGHT", ReputationWatchBar, "TOPRIGHT", 0, 0);
+	addon:ScaleFrames(bagButtons, .7);
+	addon:FadeFrames(bagButtons);
+end;
+
+
+---- SCRIPTS ----
 
 addon:SetScript("OnEvent", function(self, event, ...) 
 
 	if event == "PLAYER_ENTERING_WORLD" then
-		
-		if addonTable.HIDE_GRYPHONS then
-			MainMenuBarLeftEndCap:Hide();
-			MainMenuBarRightEndCap:Hide();
-		end;
-		
-		if addonTable.HIDE_ICON_BGS then
-			MainMenuBarTexture0:Hide();
-			MainMenuBarTexture1:Hide();
-			MainMenuBarTexture2:Hide();
-			MainMenuBarTexture3:Hide();
-		end;
-		
-		addon.MoveMultiBarBottomRight();
-		addon.MoveMultiBarBottomLeft();
-		addon.MovePetActionBarFrame();
-		
-		
-		-- Scale MainMenuBar
-		MainMenuBar:SetScale(addonTable.MAIN_BAR_SCALE);
+		addon.Main();
 	end;
 end);
-
-
----- Prepare menu/bar area for action bar
-MainMenuBarTexture2:SetTexture(MainMenuBarTexture0:GetTexture());
-MainMenuBarTexture2:SetTexCoord(MainMenuBarTexture0:GetTexCoord());
-MainMenuBarTexture3:SetTexture(MainMenuBarTexture1:GetTexture());
-MainMenuBarTexture3:SetTexCoord(MainMenuBarTexture1:GetTexCoord());
-
-
--- Hide pager
-addon:SetAllScriptsToHide(ActionBarUpButton);
-addon:SetAllScriptsToHide(ActionBarDownButton);
-MainMenuBarPageNumber:Hide();
-
-
--- Main Menu
-CharacterMicroButton:ClearAllPoints();
-CharacterMicroButton:SetPoint("TOPLEFT", ReputationWatchBar, "TOPLEFT", 50, 30);
-addon:ScaleFrames(menuButtons, .7);
-addon:FadeFrames(menuButtons);
-
-
--- Bags
-MainMenuBarBackpackButton:ClearAllPoints();
-MainMenuBarBackpackButton:SetPoint("TOPRIGHT", ReputationWatchBar, "TOPRIGHT", -50, 0);
-addon:ScaleFrames(bagButtons, .7);
-addon:FadeFrames(bagButtons);
-
