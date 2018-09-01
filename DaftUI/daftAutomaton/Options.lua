444d6a3e5f7f6a3423ff076495da7514f6a2d3cb
Tony Phipps <TonyPhipps@users.noreply.github.com>
2018-09-01 17:30:22 -0400

    Update Options.lua

--- DaftUI/daftAutomaton/Options.lua
+++ DaftUI/daftAutomaton/Options.lua
@@ -1,38 +1,32 @@
-local addonName, addonTable = ... ;
+local addonName, addonTable = ...
 
 
 -- Quality of Life Improvements
-addonTable.REPAIR = true;
-addonTable.REPAIR_GUILD = true;
-addonTable.SELL_GREYS = true;
+addonTable.REPAIR = true
+addonTable.REPAIR_GUILD = true
+addonTable.SELL_GREYS = true
 
 
 -- Friend play / multiboxing
-addonTable.ACCEPT_GROUP = true; -- Accept group invites (only from friends / guildies)
-addonTable.ACCEPT_QUESTS = false; -- Accept shared quests (only when in a group)
-addonTable.ACCEPT_SUMMON = false; -- Accept summons (only when not in combat)
-addonTable.FOLLOW = true; -- Allow command !follow to follow requester (from friends / guildies)
-addonTable.INVITE = false; -- Enable auto-inviting others to group who say trigger phrase
-addonTable.INVITE_AUTORAID = false; -- Convert to raid when group is large (only when INVITE is enabled)
-addonTable.INVITE_TRIGGER = "invite"; -- Specify auto invite trigger when INVITE is enabled
-addonTable.INVITE_VERBOSE = true; -- trigger can exist anywhere in message
-addonTable.PROMOTE_ASSISTANT = true; -- Allow command !assistant to promote requester to Raid Assistant (only from friends / guildies)
-addonTable.PROMOTE_LEADER = true; -- Allow command !leader to promote requester to group Leader (only from friends / guildies)
-addonTable.SHARE_QUESTS = true; -- Share accepted quests with group members
+addonTable.ACCEPT_GROUP = true			-- Accept group invites (only from friends / guildies)
+addonTable.ACCEPT_QUESTS = true			-- Accept shared quests
+addonTable.ACCEPT_SUMMON = false		-- Accept summons (only when not in combat)
+addonTable.FOLLOW = true				-- Allow friends/guildies to use !follow to make you follow them
+addonTable.INVITE = false				-- Enable auto-inviting others to group who say trigger phrase
+addonTable.INVITE_TRIGGER = "invite"	-- Specify a trigger word to initiate auto invite
+addonTable.INVITE_VERBOSE = true		-- Trigger can exist anywhere in message; When false, the message must be the trigger only
+addonTable.INVITE_AUTORAID = false		-- Convert to raid when group is large
+addonTable.PROMOTE_ASSISTANT = true		-- Allow command !assistant to promote requester to Raid Assistant (only from friends / guildies)
+addonTable.PROMOTE_LEADER = false		-- Allow command !leader to promote requester to group Leader (only from friends / guildies)
+addonTable.SHARE_QUESTS = true			-- Automatically share new quests
 
 
 -- PVP
-addonTable.ACCEPT_RESURRECT = true; -- Accept all resurrections (when not in combat)
-addonTable.RELEASE_PVP = true; -- Release spirit instantly (when in PvP instances)
-addonTable.RELEASE_WORLD = true; -- Release spirit instantly (when in world zones)
+addonTable.ACCEPT_RESURRECT = true		-- Accept all resurrections (when not in combat)
+addonTable.RELEASE_PVP = true			-- Release spirit instantly (when in PvP instances)
+addonTable.RELEASE_WORLD = true			-- Release spirit instantly (when in world zones)
 
 
 -- Miscellaneous
-addonTable.SCREENSHOT_ACHIEVEMENTS = true; -- Take screenshot of all achievements
-addonTable.TOGGLE_CINEMATIC_SOUND = true; -- Enable sound for cinematics; reverts settings afterward
-
-
-
---TODO:
-
-
+addonTable.SCREENSHOT_ACHIEVEMENTS = true -- Take a screenshot every time an achievement is earned
+addonTable.TOGGLE_CINEMATIC_SOUND = false -- Enable sound for cinematics; reverts settings afterward
