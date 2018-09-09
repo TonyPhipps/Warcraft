d46dee8e4fe094a5bcce674294253a3211a4fb67
unknown <anthony.james.phipps@gmail.com>
2018-09-09 17:28:52 -0400

    Default to all options off

--- DaftUI/daftUITweaks/options.lua
+++ DaftUI/daftUITweaks/options.lua
@@ -1,75 +1,106 @@
 local addonName, addonTable = ... 
 
--- Mainbar
-addonTable.MAINBAR_ALPHA = 0.9
-addonTable.EXPBAR_ALPHA = 0.9
-addonTable.LEFTBAR_ALPHA = 0.9
-addonTable.RIGHTBAR_ALPHA = 0.9
-addonTable.BAGBAR_ALPHA = 0.5
-addonTable.MENUBAR_ALPHA = 0.5
-
-addonTable.EXPBAR_SCALE = 1.0 -- 0.8 allows right click mouselook under bar!
-addonTable.MENUBAR_SCALE = 1.0
-addonTable.BAGBAR_SCALE = 1.0
-
-addonTable.SKIN_BUTTONERRORS = true
-addonTable.SKIN_FONTS = true
-
-addonTable.HIDE_HOTKEYS = false
-addonTable.HIDE_MACRONAMES = false
-addonTable.HIDE_ACTIONBAR_BG = false
-addonTable.HIDE_GRYPHONS = false
-addonTable.HIDE_BAGS = true
-addonTable.HIDE_MENU = true
-addonTable.HIDE_MENU_BG = true
-
--- Name Cast Bars
-addonTable.PLAYER_ENABLED = true
-addonTable.PLAYER_BIG_SPELL_ICON = true
-addonTable.PLAYER_HIDE_SPELL_ICON = false
-addonTable.PLAYER_TIMER = true
-addonTable.PLAYER_HIDE_PVP_ICON = true
-
-addonTable.TARGET_ENABLED = true
-addonTable.TARGET_BIG_SPELL_ICON = true
-addonTable.TARGET_HIDE_SPELL_ICON = false
-addonTable.TARGET_TIMER = true
-addonTable.TARGET_HIDE_PVP_ICON = true
-
--- Dress Up Frame
-addonTable.DRESSUP_SCALE = 1.95
-addonTable.DRESSUP_MOUSEOVER = true
-addonTable.DRESSUP_CENTER = true
-
--- Gossip
-addonTable.GOSSIP_SCALE = 1.75
-addonTable.INCLUDE_MERCHANT = true
-addonTable.INCLUDE_QUEST = true
-addonTable.INCLUDE_BOOKS = true
-
--- UI Colors (class colors by default)
-addonTable.CUSTOM = false
-addonTable.R = 0.2
-addonTable.G = 0.2
-addonTable.B = 0.2
-addonTable.A = 1
-
--- Minimap
-addonTable.MINIMAP_SCALE = 1.3
-addonTable.FADE_BUTTONS = true
-addonTable.FADE_CLOCK = true
-addonTable.FADE_MAIL = true
-addonTable.FADE_NOTICES = true
-addonTable.FADE_ZONETEXT = true
-addonTable.HIDE_ZOOM = true
-addonTable.HIDE_NORTH = true
-addonTable.MICROMENU = true
-addonTable.SCROLL_ZOOM = true
-addonTable.SKIN_BUTTONS = true
-addonTable.SKIN_ZONETEXT = true
 
 -- Full UI Fading
-addonTable.TIMETOFADEIN = 0.1
-addonTable.TIMETOFADEOUT = 10.0
-addonTable.FADEIN = 1.0
-addonTable.FADEOUT = 0.0
+addonTable.ENABLE_UIFADE = false
+    addonTable.TIMETOFADEIN = 0.1
+    addonTable.TIMETOFADEOUT = 10.0
+    addonTable.FADEIN = 1.0
+    addonTable.FADEOUT = 0.0
+
+
+-- Mainbar
+addonTable.ENABLE_MAINBAR = false
+    addonTable.MAINBAR_ALPHA = 1.0
+    addonTable.EXPBAR_ALPHA = 1.0
+    addonTable.LEFTBAR_ALPHA = 1.0
+    addonTable.RIGHTBAR_ALPHA = 1.0
+    addonTable.BAGBAR_ALPHA = 1.0
+    addonTable.MENUBAR_ALPHA = 1.0
+
+    addonTable.EXPBAR_SCALE = 1.0 -- 0.8 allows right click mouselook under bar!
+    addonTable.MENUBAR_SCALE = 1.0
+    addonTable.BAGBAR_SCALE = 1.0
+
+    addonTable.VCenterRightBars = false
+
+    addonTable.SKIN_BUTTONERRORS = false
+    addonTable.SKIN_FONTS = false
+
+    addonTable.HIDE_HOTKEYS = false
+    addonTable.HIDE_MACRONAMES = false
+    addonTable.HIDE_ACTIONBAR_BG = false
+    addonTable.HIDE_GRYPHONS = false
+    addonTable.HIDE_BAGS = false
+    addonTable.HIDE_MENU = false
+    addonTable.HIDE_MENU_BG = false
+
+
+-- UI Colors (class colors by default)
+addonTable.ENABLE_UICOLORS = false
+    addonTable.CUSTOM = true
+    addonTable.R = 0
+    addonTable.G = 0
+    addonTable.B = 0
+    addonTable.A = 1
+    
+
+-- Cast Bars
+addonTable.ENABLE_CASTBARS = false
+    addonTable.PLAYER_ENABLED = true
+        addonTable.PLAYER_BIG_SPELL_ICON = true
+        addonTable.PLAYER_HIDE_SPELL_ICON = false
+        addonTable.PLAYER_TIMER = true
+        addonTable.PLAYER_HIDE_PVP_ICON = true
+
+    addonTable.TARGET_ENABLED = true
+        addonTable.TARGET_BIG_SPELL_ICON = true
+        addonTable.TARGET_HIDE_SPELL_ICON = false
+        addonTable.TARGET_TIMER = true
+        addonTable.TARGET_HIDE_PVP_ICON = true
+
+
+-- Minimap
+addonTable.ENABLE_MINIMAP = false
+    addonTable.MINIMAP_SCALE = 1.3
+    addonTable.FADE_BUTTONS = true
+    addonTable.FADE_CLOCK = true
+    addonTable.FADE_MAIL = true
+    addonTable.FADE_NOTICES = true
+    addonTable.FADE_ZONETEXT = true
+    addonTable.HIDE_ZOOM = true
+    addonTable.HIDE_NORTH = true
+    addonTable.MICROMENU = true
+    addonTable.SCROLL_ZOOM = true
+    addonTable.SKIN_BUTTONS = true
+    addonTable.SKIN_ZONETEXT = true
+
+
+-- Dress Up Frame
+addonTable.ENABLE_DRESSUP = false
+    addonTable.DRESSUP_SCALE = 1.95
+    addonTable.DRESSUP_MOUSEOVER = true
+    addonTable.DRESSUP_CENTER = true
+
+
+-- Gossip
+addonTable.ENABLE_GOSSIP = false
+    addonTable.GOSSIP_SCALE = 1.75
+    addonTable.GOSSIP_MERCHANT = true
+    addonTable.GOSSIP_QUEST = true
+    addonTable.GOSSIP_BOOKS = true
+
+
+-- Class Icon Portraits
+addonTable.CLASS_PORTRAIT = false
+
+
+-- Class Colored Health Bars
+addonTable.CLASS_HEALTH = false
+
+
+-- Target Role Icons
+addonTable.TARGET_ROLE = false
+
+
+
