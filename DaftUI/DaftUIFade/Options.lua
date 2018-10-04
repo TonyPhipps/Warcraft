f184ed2ae42ce267f95159d03cd833f20e3c0daf
unknown <anthony.james.phipps@gmail.com>
2018-10-03 21:48:14 -0400

    rolled into daftUITweaks

--- DaftUI/DaftUIFade/Options.lua
+++
@@ -1,7 +0,0 @@
-local addonName, addonTable = ... ;
-
-addonTable.TIMETOFADEIN = 1.0;
-addonTable.FADEIN = 1.0;
-
-addonTable.TIMETOFADEOUT = 3.0;
-addonTable.FADEOUT = 0.00;
--- DaftUI/daftFrameHue/Options.lua
+++
@@ -1,7 +0,0 @@
-local addonName, addonTable = ... ;
-
-addonTable.CUSTOM = false;
-addonTable.R = 0.2;
-addonTable.G = 0.2;
-addonTable.B = 0.2;
-addonTable.A = 1;
