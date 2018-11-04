b8c826b29072392eea3047a47c0977d1601a13d5
unknown <anthony.james.phipps@gmail.com>
2018-11-03 23:39:24 -0400

    bossframe alpha work

--- DaftUI/daftUITweaks/BossFrame.lua
+++ DaftUI/daftUITweaks/BossFrame.lua
@@ -1,17 +1,18 @@
 local function MoveBossFrame()
-    Boss1TargetFrame:ClearAllPoints()
-    Boss1TargetFrame:SetPoint("TOP", UIParent, "TOP", 50, 0)
-    Boss1TargetFrame:SetScale(2)
-
-    Boss2TargetFrame:ClearAllPoints()
-    Boss2TargetFrame:SetPoint("TOPLEFT", Boss1TargetFrame, "TOPRIGHT", -200, -10)
-    Boss3TargetFrame:ClearAllPoints()
-    Boss3TargetFrame:SetPoint("TOPLEFT", Boss2TargetFrame, "TOPRIGHT", -50, 0)
-    Boss4TargetFrame:ClearAllPoints()
-    Boss4TargetFrame:SetPoint("TOPLEFT", Boss2TargetFrame, "BOTTOMLEFT", 0, 0)
-    Boss5TargetFrame:ClearAllPoints()
-    Boss5TargetFrame:SetPoint("TOPLEFT", Boss3TargetFrame, "BOTTOMLEFT", 0, 0)
+    
+    for i = 1, 5 do
+        _G["Boss"..i.."TargetFrame"]:SetScale(1.5)
+        _G["Boss"..i.."TargetFrame"]:ClearAllPoints()
+    end
+    
+    Boss1TargetFrame:SetPoint("TOPRIGHT", MinimapCluster, "BOTTOM", 100, 0)    
+    Boss2TargetFrame:SetPoint("TOPLEFT", Boss1TargetFrame, "BOTTOMLEFT", 0, 20)
+    Boss3TargetFrame:SetPoint("TOPLEFT", Boss2TargetFrame, "BOTTOMLEFT", 0, 20)
+    Boss4TargetFrame:SetPoint("TOPLEFT", Boss3TargetFrame, "BOTTOMLEFT", 0, 20)
+    Boss5TargetFrame:SetPoint("TOPLEFT", Boss4TargetFrame, "BOTTOMLEFT", 0, 20)
 end
 
-Boss1TargetFrame:HookScript("OnShow", MoveBossFrame)
-Boss1TargetFrame:HookScript("OnUpdate", MoveBossFrame)
+for i = 1, 5 do
+    _G["Boss"..i.."TargetFrame"]:HookScript("OnEvent", MoveBossFrame)
+end
+
